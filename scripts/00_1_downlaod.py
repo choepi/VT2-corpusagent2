eebe51d93ac51e561722aae50437893d0222c0b5
Tenpa <49416612+choepi@users.noreply.github.com>
2026-03-11 21:02:35 +0100

    -

---
+++ scripts/00_1_downlaod.py
@@ -0,0 +1,5 @@
+from datasets import load_dataset
+
+if __name__ == "__main__":
+    ds = load_dataset("vblagoje/cc_news", split="train")
+    ds.to_json("corpusagent2\data\raw\incoming\cc_news.jsonl.gz", compression="gzip")
